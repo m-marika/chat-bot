131627c9aa6199e0f535a004e6cc56260441c09d
m-marika <m-marika1@yandex.ru>
2023-08-18 11:59:58 +0700

    Add some button and command(menu)

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 API_TOKEN = '6434503666:AAEz2Kg3h7e_xieY0zwouMpKmA2CzVfK1HM'
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
+urlKb = InlineKeyboardMarkup(row_width=1)
+urlButton = InlineKeyboardButton(text='Blog Skillbox', url='https://skillbox.ru/media/code/')
+urlButton2 = InlineKeyboardButton(text='Lesson Skillbox', url='https://skillbox.ru/code/')
+urlKb.add(urlButton, urlButton2)
 
 
-@dp.message_handler(commands=['start'])  # Явно указываем в декораторе, на какую команду реагируем.
+@dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await message.reply(
-        "Привет!\nЯ Эхо-бот!\nОтправь мне любое сообщение, и я его повторю.")  # Так как код работает асинхронно,
-    # то обязательно пишем await.
+    kb = [
+        [
+            types.KeyboardButton(text="Can repeat it?"),
+            types.KeyboardButton(text="or this?")
+        ],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(keyboard=kb)
+
+    await message.reply("Hello!\nI'm RepeatBot!\nSend me any message and I will repeat it.",
+                        reply_markup=keyboard)
+
+
+@dp.message_handler(commands='url')
+async def url_command(message: types.Message):
+    await message.answer('Nice web:', reply_markup=urlKb)
 
 
 @dp.message_handler()  # Создаём новое событие, которое запускается в ответ на любой текст, введённый пользователем.
