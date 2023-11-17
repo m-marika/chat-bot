dfc894529da140d51ade982d747b224f2b6ba4c8
m-marika <m-marika1@yandex.ru>
2023-11-17 10:22:56 +0700

    add test

--- tests.py
+++ tests.py
@@ -1,8 +1,7 @@
 import unittest
 from unittest.mock import patch, MagicMock
-from telebot.types import User, Chat
+from telebot.types import User, Chat, InlineKeyboardMarkup, InlineKeyboardButton
 
-# Assuming `main` is your bot module with the `say_hi` function
 import main
 
 class TestBot(unittest.TestCase):
@@ -26,5 +25,32 @@ class TestBot(unittest.TestCase):
         # Assert that the bot sent the correct message
         mock_bot.send_message.assert_called_with(123, text='Hello, John!!!')
 
+
+    @patch('main.bot.send_message')
+    def test_urls_command(self, mock_send_message):
+
+        message = MagicMock()
+        message.chat.id = 123
+
+        with patch('main.InlineKeyboardMarkup') as mock_InlineKeyboardMarkup:
+            mock_InlineKeyboardMarkup.return_value = InlineKeyboardMarkup()
+            with patch('main.InlineKeyboardButton') as mock_InlineKeyboardButton:
+                mock_InlineKeyboardButton.return_value = InlineKeyboardButton(text='GitHub', url='https://github.com/m-marika')
+                main.urls(message)
+
+        # Assertions to check if send_message was called with expected arguments
+        mock_send_message.assert_called_once()
+        args, kwargs = mock_send_message.call_args
+        # self.assertEqual(kwargs['chat_id'], 123)
+        self.assertEqual(kwargs['text'], 'Visit the following links:')
+        self.assertIsInstance(kwargs['reply_markup'], InlineKeyboardMarkup)
+
+        # Check if InlineKeyboardButton was called with expected arguments
+        mock_InlineKeyboardButton.assert_any_call(text='GitHub', url='https://github.com/m-marika')
+        mock_InlineKeyboardButton.assert_any_call(text='Google', url='https://google.com')
+
+
 if __name__ == '__main__':
     unittest.main()
+
+
